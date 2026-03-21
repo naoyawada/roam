156560f00157048ae1500d87493150d1231ebbd3
naoyawada <naoya@naoyawada.com>
2026-03-21 15:28:38 -0400

    feat: add HapticService utility for tactile feedback

---
+++ Roam/Utilities/HapticService.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+enum HapticService {
+    static func selection() {
+        UISelectionFeedbackGenerator().selectionChanged()
+    }
+
+    static func light() {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+    }
+
+    static func medium() {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    }
+}
